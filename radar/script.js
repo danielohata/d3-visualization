02529c6c982c58ef9686c21a7d063bbb8306f66b
danthyl <danthyl@gmail.com>
2020-05-28 13:43:45 -0300

    inserção de exercício

---
+++ radar/script.js
@@ -0,0 +1,85 @@
+var w = 500,
+	h = 500;
+
+var colorscale = d3.scale.category10();
+
+//Legend titles
+var LegendOptions = ['Jogador 1','Jogador 2'];
+
+//Data
+var d = [
+		  [
+			{axis:"Socializador",value:0.80},
+			{axis:"Explorador",value:0.60},
+			{axis:"Conquistador",value:0.33},
+			{axis:"Assassino",value:0.27},			
+		  ],[
+			{axis:"Explorador",value:0.53},
+			{axis:"Socializador",value:0.53},
+			{axis:"Conquistador",value:0.47},
+			{axis:"Assassino",value:0.47},			
+		  ]
+		];
+
+//Options for the Radar chart, other than default
+var mycfg = {
+  w: w,
+  h: h,
+  maxValue: 1.0,
+  levels: 10,
+  ExtraWidthX: 300
+}
+
+//Call function to draw the Radar chart
+//Will expect that data is in %'s
+RadarChart.draw("#chart", d, mycfg);
+
+////////////////////////////////////////////
+/////////// Initiate legend ////////////////
+////////////////////////////////////////////
+
+var svg = d3.select('#body')
+	.selectAll('svg')
+	.append('svg')
+	.attr("width", w+300)
+	.attr("height", h)
+
+//Create the title for the legend
+var text = svg.append("text")
+	.attr("class", "title")
+	.attr('transform', 'translate(90,0)') 
+	.attr("x", w - 70)
+	.attr("y", 10)
+	.attr("font-size", "12px")
+	.attr("fill", "#404040")
+	.text("Modelo de Classificação de Bartle");
+		
+//Initiate Legend	
+var legend = svg.append("g")
+	.attr("class", "legend")
+	.attr("height", 100)
+	.attr("width", 200)
+	.attr('transform', 'translate(90,20)') 
+	;
+	//Create colour squares
+	legend.selectAll('rect')
+	  .data(LegendOptions)
+	  .enter()
+	  .append("rect")
+	  .attr("x", w - 65)
+	  .attr("y", function(d, i){ return i * 20;})
+	  .attr("width", 10)
+	  .attr("height", 10)
+	  .style("fill", function(d, i){ return colorscale(i);})
+	  ;
+	//Create text next to squares
+	legend.selectAll('text')
+	  .data(LegendOptions)
+	  .enter()
+	  .append("text")
+	  .attr("x", w - 52)
+	  .attr("y", function(d, i){ return i * 20 + 9;})
+	  .attr("font-size", "11px")
+	  .attr("fill", "#737373")
+	  .text(function(d) { return d; })
+	  ;
